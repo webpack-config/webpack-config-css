40ebeb1ea1c9c161b39eb58351925c609e17c8e2
Neal Granger <neal@metalab.co>
2017-03-01 15:03:32 -0800

    Use config `context` to resolve PostCSS config.

--- src/css.webpack.config.js
+++ src/css.webpack.config.js
@@ -1,14 +1,12 @@
 import {existsSync} from 'fs';
-import {dirname, join} from 'path';
+import {join} from 'path';
 import compose from 'lodash/fp/flowRight';
 import identity from 'lodash/fp/identity';
 import isPlainObject from 'lodash/fp/isPlainObject';
 import {loader, plugin} from 'webpack-partial';
-import nearest from 'find-nearest-file';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const PRODUCTION = process.env.NODE_ENV === 'production';
-const ROOT = dirname(nearest('package.json'));
 
 const hasPostcssConfig = (root) =>
   existsSync(`${root}/postcss.config.js`) ||
@@ -17,6 +15,47 @@ const hasPostcssConfig = (root) =>
   existsSync(`${root}/.postcssrc.js`) ||
   existsSync(`${root}/.postcssrc`);
 
+const getPostCSSOptions = (postcss, context) => {
+  if (isPlainObject(postcss)) {
+    // The `postcss` option is an object. Pass it through as the
+    // `postcss-loader` options.
+    return postcss;
+  }
+
+  if (hasPostcssConfig(context)) {
+    // There is a PostCSS config file at the project root. Return an empty
+    // options object to allow the config to be used.
+    return {};
+  }
+
+  if (hasPostcssConfig(join(context, 'config', 'postcss'))) {
+    // There is a PostCSS config file in the `/config/postcss` directory.
+    // Return an options object referencing it.
+    return {
+      config: join(context, 'config', 'postcss'),
+    };
+  }
+
+  // Return an options object referencing the default PostCSS config that is
+  // part of this config partial.
+  return {
+    config: __dirname,
+  };
+};
+
+/**
+ * CSS Webpack loader partial.
+ *
+ * @param {Boolean|Object} options.extract Enable/disable Extract Text Plugin.
+ * An object value is used as options for the Extract Text Plugin constructor.
+ * @param {Boolean} options.minimize Enable/disable cssnano minification.
+ * @param {Boolean} options.jsStyles Enable/disable `css-js-loader`.
+ * @param {Boolean|Object} options.postcss Enable/disable `postcss-loader`.
+ * An object value is used as options for `postcss-loader`.
+ * @param {Boolean} options.modules Enable/disable CSS Modules.
+ * @param {String} options.localIdentName CSS Modules local identifier template.
+ * @returns {Function} Webpack config partial.
+ */
 export default ({
   extract = PRODUCTION,
   minimize = PRODUCTION,
@@ -43,22 +82,6 @@ export default ({
 
   const query = {modules, localIdentName, minimize, importLoaders, ...options};
 
-  let postcssOptions;
-
-  if (isPlainObject(postcss)) {
-    postcssOptions = postcss;
-  } else if (hasPostcssConfig(ROOT)) {
-    postcssOptions = {};
-  } else if (hasPostcssConfig(join(ROOT, 'config', 'postcss'))) {
-    postcssOptions = {
-      config: join(ROOT, 'config', 'postcss'),
-    };
-  } else {
-    postcssOptions = {
-      config: __dirname,
-    };
-  }
-
   return (config) => compose(
     plugin(extractor),
     jsStyles ? loader({
@@ -68,7 +91,7 @@ export default ({
     postcss ? loader({
       test: IS_STYLE,
       loader: require.resolve('postcss-loader'),
-      options: postcssOptions,
+      options: getPostCSSOptions(postcss, config.context),
     }) : identity,
     config.target === 'web'
       ? loader({
